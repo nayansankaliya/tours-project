4bf3ac830f706760901c76e673a6b555d74d2770
Vaibhav <vaibhav.chavda@digiqt.com>
2025-02-28 12:11:55 +0530

    fourth commit

--- Tours/tours.js
+++ Tours/tours.js
@@ -1,3 +1,51 @@
+let clcImg1 = document.getElementById('img1');
+
+clcImg1.addEventListener("click", () => {
+    window.location.href = "/detail/1tour.html"
+})
+let clcImg2 = document.getElementById('img2');
+
+clcImg2.addEventListener("click", () => {
+    window.location.href = "/detail/2tour.html"
+})
+let clcImg3 = document.getElementById('img3');
+
+clcImg3.addEventListener("click", () => {
+    window.location.href = "/detail/3tour.html"
+})
+let clcImg4 = document.getElementById('img4');
+
+clcImg4.addEventListener("click", () => {
+    window.location.href = "/detail/4tour.html"
+})
+let clcImg5 = document.getElementById('img5');
+
+clcImg5.addEventListener("click", () => {
+    window.location.href = "/detail/5tour.html"
+})
+
+let clcImg6 = document.getElementById('img6');
+
+clcImg6.addEventListener("click", () => {
+    window.location.href = "/detail/6tour.html"
+})
+
+let clcImg7 = document.getElementById('img7');
+
+clcImg7.addEventListener("click", () => {
+    window.location.href = "/detail/7tour.html"
+})
+let clcImg8 = document.getElementById('img8');
+
+clcImg8.addEventListener("click", () => {
+    window.location.href = "/detail/8tour.html"
+})
+let clcImg9 = document.getElementById('img9');
+
+clcImg9.addEventListener("click", () => {
+    window.location.href = "/detail/9tour.html"
+})
+
 let btnCustom = document.getElementById('custom');
 
 btnCustom.addEventListener("click", () => {
--- tours/tours.js
+++ tours/tours.js
@@ -10,6 +10,37 @@ let btnSignup = document.getElementById('signup');
 btnSignup.addEventListener("click", () => {
     window.location.href = "/sign up/sign.html"
 })
+let clcImg1 = document.getElementById('img1');
+
+clcImg1.addEventListener("click", () => {
+    window.location.href = "/detail/1tour.html"
+})
+let clcImg2 = document.getElementById('img2');
+
+clcImg2.addEventListener("click", () => {
+    window.location.href = "/detail/2tour.html"
+})
+let clcImg3 = document.getElementById('img3');
+
+clcImg3.addEventListener("click", () => {
+    window.location.href = "/detail/3tour.html"
+})
+let clcImg4 = document.getElementById('img4');
+
+clcImg4.addEventListener("click", () => {
+    window.location.href = "/detail/4tour.html"
+})
+let clcImg5 = document.getElementById('img5');
+
+clcImg5.addEventListener("click", () => {
+    window.location.href = "/detail/5tour.html"
+})
+
+let clcImg6 = document.getElementById('img6');
+
+clcImg6.addEventListener("click", () => {
+    window.location.href = "/detail/6tour.html"
+})
 
 let btnCustom = document.getElementById('custom');
 
